d8f738b633dcba4e84023674761e4fc7039bd1f8
skh1 <s50959402@gmail.com>
2022-01-21 14:31:16 +0900

    h 인덱스 정렬

--- src/test_algorithms/Test05.java
+++ src/test_algorithms/Test05.java
@@ -13,9 +13,9 @@ public class Test05 {
 			int a = commands[i][0];
 			int b = commands[i][1];
 			int c = commands[i][2];
-			int[] array2 = Arrays.copyOfRange(array, a-1, b);
-			
-			Arrays.sort(array2);
+			int[] array2 = Arrays.copyOfRange(array, a-1, b); // copyOfRange = (복사할 배열,시작인덱스,배열크기)
+			System.out.println(Arrays.toString(array2));
+			Arrays.sort(array2); //Arrays.sort = 배열을 오름차순으로 정렬
 			
 			answer[i] = array2[c-1];
 		}
---
+++ src/test_algorithms/Test07.java
@@ -0,0 +1,54 @@
+package test_algorithms;
+
+//import java.util.Arrays;
+
+
+public class Test07 {
+	public static int solution(int[] citations) {
+        
+//정렬을 썼을 때		
+		
+//		Arrays.sort(citations);
+//		int answer = 0;
+//		int cnt=0;
+//		for(int i=citations.length; i>0; i--) {
+//            cnt++;
+//			if(citations[i-1]>=cnt) {
+//				answer = cnt;
+//			}
+//			
+//			
+//		}
+//		
+//		
+//        return answer;
+		
+//정렬을 쓰지 않았을 때		
+		
+		int answer = 0;
+		
+		for(int i=1; i<=citations.length; i++) {
+			int cnt=0;
+			for(int j=0; j<citations.length; j++) {
+				if(citations[j]>=i) {
+					cnt++;
+				}
+			}
+			
+			if(cnt>=i) {
+				answer = i;
+			}
+			
+		}
+		
+		
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		int[] citations = {3, 0, 6, 1, 5};
+		
+		System.out.println(solution(citations));
+	}
+}
